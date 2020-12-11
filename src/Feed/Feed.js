e2434d1cec10a867b4593502d2e8e2820f855036
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-11 20:50:56 +0200

    finished sidebar

---
+++ src/Feed/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './Feed.css'
+const Feed = () => {
+    return (
+        <div className="feed">
+             <h2>Feed</h2>
+        </div>
+       
+    )
+}
+
+export default Feed;
