55fb124b396cb32536e330ec21fc28eea8023bcd
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-12 11:44:38 +0200

    fininshed home

---
+++ src/Widgets/Widgets.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Widgets.css'
+const Widgets = () => {
+    return (
+        <div className="widgets">
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+        <h2>Widgets</h2>
+
+        </div>
+    )
+}
+
+export default Widgets;
