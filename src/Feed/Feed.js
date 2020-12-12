9be15f570455555ea39261712cd180d77e13ea72
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-12 11:36:10 +0200

    fininshed home

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,9 +1,42 @@
 import React from 'react'; 
+import Home from '../Home/Home'
+import TweetBox from '../TweetBox/TweetBox'
 import './Feed.css'
 const Feed = () => {
     return (
         <div className="feed">
+            <Home />
+            <TweetBox />
              <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+            <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             {/* <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2>
+             <h2>Feed</h2> */}
         </div>
        
     )
