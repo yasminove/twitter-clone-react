da4bf8ad02e67b944e083568a486a059a7d2c404
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-12 13:11:20 +0200

    fininshed home

--- src/TweetBox/TweetBox.js
+++ src/TweetBox/TweetBox.js
@@ -1,15 +1,24 @@
-import React from 'react'; 
-import './TweetBox.css'
-import { Avatar, Button } from '@material-ui/core'
+import React from 'react';
+import './TweetBox.css';
+import Image from '../images/yasmin.jpeg';
+import { Avatar, Button } from '@material-ui/core';
 
 const TweetBox = () => {
-    return (
-        <div className="tweetBox">
-            <Avatar />
-            <input type="text" placeholder="What's happening"/>
-            <Button className="tweetBox_btn" variant="contained">Tweet</Button>
+  return (
+    <div className='tweetBox'>
+      <form action=''>
+        <div className='tweetBox__input'>
+          <Avatar src={Image}/>
+          <input type='text' placeholder="What's happening" />
+          
         </div>
-    )
-}
+        <input className="tweetBox__image" type='text' placeholder="Optional: Add an image" />
+        <Button className='tweetBox_btn' variant='contained'>
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default TweetBox;
