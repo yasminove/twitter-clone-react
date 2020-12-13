fe78e5187b75fe86f8127c44c808890fadd188bf
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-13 22:01:22 +0200

    finished firebase

--- src/Tweet/Tweet.js
+++ src/Tweet/Tweet.js
@@ -7,15 +7,34 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import db from '../firbase';
+
+import  {docId}  from '../TweetBox/TweetBox'
 
 import Person from '../images/person.jpg'
 
-const Tweet = ({ profilePic, displayName, username, text, image }) => {
+const Tweet = ({ profilePic, displayName, username, text, image, heartCount }) => {
     const [ heartConut, setHeartConut ] = useState(0); 
     
     const handleHeartClick = () => {
+        console.log(docId, 'docId');
+        
         setHeartConut(heartConut + 1)
+        // const doc = db.collection("posts").doc(docId); 
+        // console.log(doc, 'doc777777');
+        // console.log(doc.id, 'doc777777');
+        const doc = db.collection('posts').doc(docId);
+        // console.log(heartConut, 'heartConut');
+        console.log(heartConut, 'heartConutttttttttttttttttttt');
+        doc.update({
+            heart: heartConut + 1,
+            displayName: "The GREAT"
+
+        })    
+        console.log(doc, 'doc');    
     }
+
+    // console.log(heartConut, 'heartConutheartConutheartConutheartConutheartConut');
 
     const handleFilledHeartClick = () => {
         setHeartConut(heartConut-1)
@@ -35,8 +54,11 @@ const Tweet = ({ profilePic, displayName, username, text, image }) => {
                 <span className="tweet__username">@{username}</span>
             </h2>
                 <p>{text}</p>
-
-                <img className="tweet__image" style={{width: '201px', height: '175px'}} src={image} />
+                {image ? (
+                    <img className="tweet__image" style={{width: '201px', height: '175px'}} src={image} />
+                )
+                : null}
+                
 
             </div>
             </div>
@@ -44,7 +66,7 @@ const Tweet = ({ profilePic, displayName, username, text, image }) => {
             <ChatBubbleOutlineIcon />
             <RepeatIcon />
             { heartConut === 0 || heartConut % 2 === 0 ? <FavoriteBorderIcon onClick={handleHeartClick}/> :  <FavoriteIcon onClick={handleFilledHeartClick} style={{color: '#e0245e'}}/>}
-            { heartConut > 0 ?  <span style={{marginLeft: '-107px'}}>{heartConut}</span> : null}
+            { heartConut > 0 ?  <span style={{marginLeft: '-107px'}}>{heartCount}</span> : null}
             <PublishIcon />
 
             </div>
